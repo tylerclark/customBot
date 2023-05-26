92f0298dbb75d9044f575885b29a2ea2c92922cf
Sujal Samai <87236576+SujalSamai@users.noreply.github.com>
2023-05-26 17:00:39 +0530

    Bug Fix

--- chatbot.py
+++ chatbot.py
@@ -11,7 +11,7 @@ from langchain.llms import OpenAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.callbacks import get_openai_callback
 
-openai.api_key == st.secrets["OPENAI_API_KEY"]
+openai.api_key = st.secrets["OPENAI_API_KEY"]
 
 
 def main():
