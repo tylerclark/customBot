3fbfbb6283a9ba8c28e215790ceec86c97935f09
Sujal Samai <87236576+SujalSamai@users.noreply.github.com>
2023-05-27 08:47:30 +0530

    Removed config file

--- chatbot.py
+++ chatbot.py
@@ -2,7 +2,7 @@ import os
 import streamlit as st
 import openai
 import pickle
-import config
+# import config
 from PyPDF2 import PdfReader
 from streamlit_extras.add_vertical_space import add_vertical_space
 from langchain.text_splitter import RecursiveCharacterTextSplitter
@@ -14,8 +14,14 @@ from langchain.callbacks import get_openai_callback
 from azure.identity import DefaultAzureCredential
 from azure.keyvault.secrets import SecretClient
 
+KEY_VAULT_NAME = "WebAppKeys"
+KeyVault_URI = f"https://{KEY_VAULT_NAME}.vault.azure.net/"
 
-openai.api_key = config.OPENAI_API_KEY
+_credential = DefaultAzureCredential()
+_sc = SecretClient(vault_url=KeyVault_URI, credential=_credential)
+OPENAI_API_KEY = _sc.get_secret("openai-api-key").value
+
+openai.api_key = OPENAI_API_KEY
 
 
 def main():
@@ -24,7 +30,6 @@ def main():
     # upload a PDF file
     pdf = st.file_uploader("Upload your PDF", type='pdf')
 
-    # st.write(pdf)
     if pdf is not None:
         pdf_reader = PdfReader(pdf)
 
@@ -42,24 +47,21 @@ def main():
         # # embeddings
         store_name = pdf.name[:-4]
         st.success("Successfully uploaded: "f'{store_name}')
-        # st.write(chunks)
 
-        if os.path.exists(f"{store_name}.pkl"):
-            with open(f"{store_name}.pkl", "rb") as f:
-                VectorStore = pickle.load(f)
-            # st.write('Embeddings Loaded from the Disk')s
-        else:
-            embeddings = OpenAIEmbeddings()
-            VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
-            with open(f"{store_name}.pkl", "wb") as f:
-                pickle.dump(VectorStore, f)
+        # if os.path.exists(f"{store_name}.pkl"):
+        #     with open(f"{store_name}.pkl", "rb") as f:
+        #         VectorStore = pickle.load(f)
+        # else:
+        #     embeddings = OpenAIEmbeddings()
+        #     VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
+        #     with open(f"{store_name}.pkl", "wb") as f:
+        #         pickle.dump(VectorStore, f)
 
-        # embeddings = OpenAIEmbeddings()
-        # VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
+        embeddings = OpenAIEmbeddings()
+        VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
 
         # Accept user questions/query
         query = st.text_input("Ask questions about your PDF file:")
-        # st.write(query)
 
         if query:
             k = 3  # Number of nearest neighbors to retrieve
